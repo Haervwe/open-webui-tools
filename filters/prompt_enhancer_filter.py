0f7b0093cc938a8fac4eaf75a8a99e6f6433b028
Haervwe <juan-jose-007@hotmail.com>
2025-03-22 13:39:07 -0300

    Bump version to 0.5.1 for Prompt Enhancer filter

--- filters/prompt_enhancer_filter.py
+++ filters/prompt_enhancer_filter.py
@@ -3,7 +3,7 @@ title: Prompt Enhancer
 author: Haervwe
 author_url: https://github.com/Haervwe
 funding_url: https://github.com/Haervwe/open-webui-tools
-version: 0.5
+version: 0.5.1
 """
 
 import logging
