f58a640916b2478613d8454e6b71464a244d8e20
Haervwe <juan-jose-007@hotmail.com>
2025-03-22 22:41:11 -0300

    Add important note regarding usage of enhanced prompt valve setting in prompt enhancer filter

--- filters/prompt_enhancer_filter.py
+++ filters/prompt_enhancer_filter.py
@@ -4,6 +4,7 @@ author: Haervwe
 author_url: https://github.com/Haervwe
 funding_url: https://github.com/Haervwe/open-webui-tools
 version: 0.5.2
+important note: if you are going to sue this filter with custom pipes, do not use the show enhanced prompt valve setting 
 """
 
 import logging
